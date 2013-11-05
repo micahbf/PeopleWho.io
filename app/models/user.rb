74dcd03300883a3085ffec6e3ea1bc4259787060
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 10:12:38 -0800

    Add session methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: true
   validates :password, presence: true, on: :create
+
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token || self.class.generate_session_token
+  end
 end
