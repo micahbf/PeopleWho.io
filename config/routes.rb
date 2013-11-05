3b46c15348b54a6f90cfb3d7ac3bced84c29f621
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 11:41:00 -0800

    Generate BillsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 BillTracker::Application.routes.draw do
+  get "bills/new"
+
+  get "bills/create"
+
+  get "bills/edit"
+
+  get "bills/update"
+
+  get "bills/destroy"
+
+  get "bills/index"
+
+  get "bills/show"
+
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
 end
