238da69816f853aac85755f5db5d5d7fb2de9fc6
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 13:24:38 -0800

    Create resourceful bills routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 BillTracker::Application.routes.draw do
-  get "bills/new"
-
-  get "bills/create"
-
-  get "bills/edit"
-
-  get "bills/update"
-
-  get "bills/destroy"
-
-  get "bills/index"
-
-  get "bills/show"
-
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
+  resources :bills
 end
