7c3c06eadf9bb25965fd44e0a8c04127821c5803
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-04 17:04:30 -0800

    Initial Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BillTracker::Application.config.secret_token = '39f963bc9088a2978a49cbb95cfa209a1f24c9fdfd09bc8d93d9fe681abc9d66ab76d1fee3ae8963f8b049e7c851c5d8528de265609215891487514eab0e24c1'
