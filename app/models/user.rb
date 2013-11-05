aa4e63cf5d165a34622edb6913bad1dd35715bab
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 14:35:50 -0800

    Fix session_token assignment in #ensure_session_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,6 @@ class User < ActiveRecord::Base
   private
 
   def ensure_session_token
-    self.session_token || self.class.generate_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 end
