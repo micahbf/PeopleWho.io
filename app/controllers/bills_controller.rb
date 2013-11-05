ca4686d625a3415142db99d4e7e0609b712acc70
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 11:51:17 -0800

    Add #new and #create actions

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,8 +1,18 @@
 class BillsController < ApplicationController
   def new
+    @bill = Bill.new
+    render :new
   end
 
   def create
+    @bill = Bill.new(params[:bill])
+    if @bill.save
+      flash[:notices] = "Bill saved."
+      redirect_to :index
+    else
+      flash.now[:errors] = @bill.errors.full_messages
+      render :new
+    end
   end
 
   def edit
