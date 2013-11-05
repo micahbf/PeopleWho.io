b9395ae61381b43b4f79efd9c31f06c877d213ef
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 11:30:45 -0800

    Add basic BillSplit validations

--- app/models/bill_split.rb
+++ app/models/bill_split.rb
@@ -1,6 +1,8 @@
 class BillSplit < ActiveRecord::Base
   attr_accessible :amount, :bill_id, :debtor_id, :paid
 
+  validates :amount, :bill, :debtor, :paid, presence: true
+
   belongs_to :bill
   belongs_to :debtor, class_name: "User"
 end
