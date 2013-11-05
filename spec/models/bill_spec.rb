ab2a57fb3eafe3248a36957c581186ec4d451dff
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 11:18:47 -0800

    Create Bill validation specs

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Bill do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    let(:bill) { FactoryGirl.build(:bill) }
+    it "should have a valid factory" do
+      expect(bill).to be_valid
+    end
+
+    it "must have an owner" do
+      bill.owner_id = nil
+      expect(bill).not_to be_valid
+    end
+
+    it "must have a total amount" do
+      bill.total = bill
+      expect(bill).not_to be_valid
+    end
+
+    it "must have at least one bill_split" do
+      bill.bill_splits = []
+      expect(bill).not_to be_valid
+    end
+
+    its "should have a total > the number of bill_splits" do
+      bill.total = 1
+      expect(bill).not_to be_valid
+    end
+  end
 end
