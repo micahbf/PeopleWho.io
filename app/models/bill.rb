a8eb1a68eb10c2681188f00a599bcdfcb9025360
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 14:08:56 -0800

    Change bill_splits to bill_splits_attributes

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,5 +1,5 @@
 class Bill < ActiveRecord::Base
-  attr_accessible :description, :owner_id, :total, :decimal_total, :bill_splits
+  attr_accessible :description, :owner_id, :total, :decimal_total, :bill_splits_attributes
 
   validates :owner, :total, presence: true
   validate :total_greater_than_num_bill_splits
