6593d55b62a7347670d00b1a632ad4a9aa4ac28f
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 13:42:01 -0800

    Add decimal_amount getter and setter

--- app/models/bill_split.rb
+++ app/models/bill_split.rb
@@ -1,8 +1,20 @@
 class BillSplit < ActiveRecord::Base
-  attr_accessible :amount, :bill_id, :debtor_id, :paid
+  attr_accessible :amount, :bill_id, :debtor_id, :paid, :decimal_amount
 
   validates :amount, :bill, :debtor, :paid, presence: true
 
   belongs_to :bill
   belongs_to :debtor, class_name: "User"
+
+  def decimal_amount
+    if amount
+      return (amount / 100).round(2)
+    else
+      return nil
+    end
+  end
+
+  def decimal_amount= (decimal)
+    amount = (decimal * 100).floor
+  end
 end
