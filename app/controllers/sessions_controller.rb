8fb7f15ab67c3c02d4a3351e197e69e3203b58cc
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 10:22:53 -0800

    Add Session #new, #create, #destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+  before_filter :require_no_current_user!, :only => [:create, :new]
+  before_filter :require_current_user!, :only => [:destroy]
+  
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      render :json => "Invalid email/password"
+    else
+      self.current_user = user
+      redirect_to user_url(user)
+    end
+  end
+
+  def destroy
+    logout_current_user!
+    redirect_to new_session_url
+  end
 end
