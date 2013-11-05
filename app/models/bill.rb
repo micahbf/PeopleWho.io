34075de635662860dbf973c3b5ef8d446f361985
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 11:38:18 -0800

    Add bill splits sum < total validation

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -3,6 +3,7 @@ class Bill < ActiveRecord::Base
 
   validates :owner, :total, presence: true
   validate :total_greater_than_num_bill_splits
+  validate :bill_splits_sum_less_than_total
 
   belongs_to :owner, class_name: "User"
 
@@ -13,6 +14,12 @@ class Bill < ActiveRecord::Base
   def total_greater_than_num_bill_splits
     if (total && total <= bill_splits.count)
       errors[:total] << "must be greater than the number of splits"
+    end
+  end
+
+  def bill_splits_sum_less_than_total
+    if (!bill_splits.empty? && bill_splits.sum(:amount) > total)
+      errors[:total] << "must be greater than the sum of the splits"
     end
   end
 end
