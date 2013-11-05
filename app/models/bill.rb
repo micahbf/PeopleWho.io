00325c6c58bb24fcbcd87d0234ed0626ad6976c3
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 11:28:39 -0800

    Add Bill validations

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,7 +1,18 @@
 class Bill < ActiveRecord::Base
   attr_accessible :description, :owner_id, :total
 
+  validates :owner, :total, presence: true
+  validate :total_greater_than_num_bill_splits
+
   belongs_to :owner, class_name: "User"
 
   has_many :bill_splits
+
+  private
+
+  def total_greater_than_num_bill_splits
+    if (total && total <= bill_splits.count)
+      errors[:total] << "must be greater than the number of splits"
+    end
+  end
 end
