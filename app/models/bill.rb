d4c748bd4f71ffc6805b45fdc976e86e5d3f1dd3
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-06 12:00:37 -0800

    Add description presence validation

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,7 +1,7 @@
 class Bill < ActiveRecord::Base
   attr_accessible :description, :owner_id, :total, :decimal_total, :bill_splits_attributes
 
-  validates :owner, :total, presence: true
+  validates :owner, :total, :description, presence: true
   validate :total_greater_than_num_bill_splits
   validate :bill_splits_sum_less_than_total
 
