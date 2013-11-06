5949d92ce3358910e5b8af125e556f0cbae5d868
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-05 17:54:54 -0800

    Add users#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 BillTracker::Application.routes.draw do
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
   resources :bills
 end
