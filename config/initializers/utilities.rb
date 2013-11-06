5b424f97a23814275c599201e5d7ccdc3b64ab2d
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-06 11:00:22 -0800

    Change methods to be on module

--- config/initializers/utilities.rb
+++ config/initializers/utilities.rb
@@ -1,9 +1,9 @@
 module Utilities
-  def int_to_decimal(int)
+  def self.int_to_decimal(int)
     "%0.2f" % (int / 100).round(2)
   end
 
-  def decimal_to_int(decimal)
+  def self.decimal_to_int(decimal)
     (decimal.to_f * 100).floor
   end
 end
