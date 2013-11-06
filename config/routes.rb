1d2f2b80f0890c835c4423e0f856678662643323
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-06 14:22:44 -0800

    Add settle_user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 BillTracker::Application.routes.draw do
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+    member do
+      post :settle
+    end
+  end
   resource :session, only: [:new, :create, :destroy]
   resources :bills
 end
