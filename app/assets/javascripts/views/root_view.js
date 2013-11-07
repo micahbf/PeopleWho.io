faafd886e35183fde180a3fedd1b0e2ee39b9d37
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-07 15:09:27 -0800

    Add new bill form into root view

--- app/assets/javascripts/views/root_view.js
+++ app/assets/javascripts/views/root_view.js
@@ -2,6 +2,8 @@ BT.Views.RootView = Backbone.View.extend({
   render: function () {
     var balancesView = new BT.Views.UserBalanceView();
     this.$el.html(balancesView.render().$el);
+    var newBillFormView = new BT.Views.NewBillFormView();
+    this.$el.prepend(newBillFormView.render().$el);
     return this;
   }
 });
