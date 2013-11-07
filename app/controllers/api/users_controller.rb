44ae62acfbe64dd35546e801c8f3d16692cad4aa
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-06 17:16:46 -0800

    Add #show action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,10 @@
 class Api::UsersController < ApplicationController
+  respond_to :json
+
+  def show
+    @user = User.find(params[:id])
+    @splits = current_user.splits_with(@user)
+    @balance = BillSplit.sum_for_user(@splits, current_user)
+    render "show"
+  end
 end
