6965607366c98a0c80a2aa423e338f2400706b45
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-07 09:59:52 -0800

    backbone:install

---
+++ app/assets/javascripts/bill_tracker.js
@@ -0,0 +1,13 @@
+window.BillTracker = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  BillTracker.initialize();
+});
