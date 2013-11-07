22d3f0e497c87e12f1a5d7712d6bce6d418ba7f4
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-07 11:52:36 -0800

    Factor balances into its own view

--- app/assets/javascripts/views/root_view.js
+++ app/assets/javascripts/views/root_view.js
@@ -1,25 +1,7 @@
 BT.Views.RootView = Backbone.View.extend({
-  userBalancesTemplate: JST['users/balances'],
-
   render: function () {
-    var owedUsers = {};
-    var owingUsers = {};
-
-    _.each(BT.balances, function (balance, user_id) {
-      var dispName = BT.users.get(user_id).displayName();
-      if (balance < 0) {
-        owedUsers[dispName] = BT.int_to_dec(balance);
-      } else {
-        owingUsers[dispName] = BT.int_to_dec(balance);
-      }
-    });
-
-    var renderedBalances = this.userBalancesTemplate({
-      owedUsers: owedUsers,
-      owingUsers: owingUsers
-    });
-
-    this.$el.html(renderedBalances);
+    var balancesView = new BT.Views.UserBalanceView();
+    this.$el.html(balancesView.render().$el);
     return this;
   }
 });
---
+++ app/assets/javascripts/views/users/user_balance_view.js
@@ -0,0 +1,25 @@
+BT.Views.UserBalanceView = Backbone.View.extend({
+  template: JST['users/balances'],
+
+  render: function () {
+    var owedUsers = {};
+    var owingUsers = {};
+
+    _.each(BT.balances, function (balance, user_id) {
+      var dispName = BT.users.get(user_id).displayName();
+      if (balance < 0) {
+        owedUsers[dispName] = BT.int_to_dec(balance);
+      } else {
+        owingUsers[dispName] = BT.int_to_dec(balance);
+      }
+    });
+
+    var renderedBalances = this.template({
+      owedUsers: owedUsers,
+      owingUsers: owingUsers
+    });
+
+    this.$el.html(renderedBalances);
+    return this;
+  }
+});
