07c0b81f767d7593e466b61b92d1e70afd2aa613
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-07 09:36:00 -0800

    Add #create and #show actions

--- app/controllers/api/bills_controller.rb
+++ app/controllers/api/bills_controller.rb
@@ -1,2 +1,20 @@
 class Api::BillsController < ApplicationController
+  respond_to :json
+
+  def create
+    @bill = Bill.new(params[:bill])
+    @bill.owner_id = current_user.owner_id
+
+    if @bill.save
+      render "show"
+    else
+      render json: { errors: @bill.errors.full_messages },
+             status: 422
+    end
+  end
+
+  def show
+    @bill = Bill.where(id: params[:id]).includes(bill_splits: :debtor).first
+    render "show"
+  end
 end
