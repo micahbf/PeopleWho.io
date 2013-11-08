3b7011517fb3f8b01d4e59b0eabb4d560ba105a9
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-07 17:34:21 -0800

    Add #initialize, #parse, #toJSON

--- app/assets/javascripts/models/bill.js
+++ app/assets/javascripts/models/bill.js
@@ -1,1 +1,24 @@
-BT.Models.Bill = Backbone.Model.extend({});
+BT.Models.Bill = Backbone.Model.extend({
+  initialize: function () {
+    this.billSplits = this.billSplits || new BT.Collections.BillSplits([], {
+      bill: this
+    });
+  },
+
+  parse: function (serverAttrs) {
+    if (serverAttrs.bill_splits) {
+      this.billSplits.add(serverAttrs.bill_splits);
+
+      delete serverAttrs.billSplits;
+    }
+
+    return serverAttrs;
+  },
+
+  toJSON: function () {
+    var json = _.extend({}, this.attributes);
+    json.bill_splits = this.billSplits.toJSON;
+
+    return json;
+  }
+});
