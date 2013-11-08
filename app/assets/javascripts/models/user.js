da9a90686140f34dd88a1f32b763b344c088372e
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-08 13:32:36 -0800

    Add #parse

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,9 +1,15 @@
 BT.Models.User = Backbone.Model.extend({
+  collection: BT.Collections.Users,
+  
   displayName: function () {
     if (this.get("name")) {
       return this.escape("name");
     } else {
       return this.escape("email");
     }
+  },
+
+  parse: function(serverAttrs) {
+    return serverAttrs.user;
   }
 });
