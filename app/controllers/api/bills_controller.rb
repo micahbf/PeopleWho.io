9c18db00eaaa454291046776a53f202714a89348
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-07 17:48:11 -0800

    Add #index action

--- app/controllers/api/bills_controller.rb
+++ app/controllers/api/bills_controller.rb
@@ -17,4 +17,12 @@ class Api::BillsController < ApplicationController
     @bill = Bill.where(id: params[:id]).includes(bill_splits: :debtor).first
     render "show"
   end
+
+  def index
+    own_bills = current_user.paid_bills.includes(:bill_splits)
+    others_bills = Bill.includes(:bill_splits).where("bill_splits.debtor_id" => current_user.id)
+
+    @bills = own_bills.concat(others_bills)
+    render "index"
+  end
 end
