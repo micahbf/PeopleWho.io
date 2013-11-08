418ba753531b70c5c97271775dc2b44ec302f4a4
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-08 11:59:17 -0800

    Add current_user_id bootstrapping

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def root
     if current_user
+      @current_user_id = current_user.id
       @users = User.all
       @users_with_balances = current_user.user_ids_with_outstanding_balance
       render :root
