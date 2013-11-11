88a6b2be5570e19351ed705428b3aff258db9ead
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-11 14:17:48 -0800

    Add bills association

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -4,6 +4,7 @@ class UserGroup < ActiveRecord::Base
   validates :name, presence: true
 
   has_many :user_group_memberships, foreign_key: :group_id, inverse_of: :group
-
   has_many :users, through: :user_group_memberships
+
+  has_many :bills
 end
