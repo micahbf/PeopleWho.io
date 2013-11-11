c56ec5c444681157f9c0a230f71d49178c24cb14
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-11 14:48:04 -0800

    Initial api user group actions

--- app/controllers/api/user_groups_controller.rb
+++ app/controllers/api/user_groups_controller.rb
@@ -1,2 +1,28 @@
 class Api::UserGroupsController < ApplicationController
+  def create
+    @group = UserGroup.new(params[:user_group])
+    @group.user_ids << current_user.id
+
+    if @group.save
+      render json: @group, methods: [:user_ids]
+    else
+      render json: { errors: @group.errors.full_messages }, status: 422
+    end
+  end
+
+  def show
+    @group = UserGroup.find(params[:id])
+    render json: @group, methods: [:user_ids]
+  end
+
+  def update
+    @group = UserGroup.find(params[:id])
+    @group.update_attributes(params[:user_group])
+
+    if @group.save
+      render json: @group, methods: [:user_ids]
+    else
+      render json: { errors: @group.errors.full_messages }, status: 422
+    end
+  end      
 end
