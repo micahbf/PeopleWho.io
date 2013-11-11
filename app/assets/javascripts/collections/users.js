d5061a75aa9d7e3053cfeb295eb48c7b9ab9f1a2
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-10 17:35:18 -0800

    Add #parse to get past object root

--- app/assets/javascripts/collections/users.js
+++ app/assets/javascripts/collections/users.js
@@ -1,4 +1,7 @@
 BT.Collections.Users = Backbone.Collection.extend({
   url: "/api/users",
-  model: BT.Models.User
+  model: BT.Models.User,
+  parse: function (serverAttrs) {
+    return serverAttrs.users;
+  }
 });
