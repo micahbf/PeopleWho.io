03f8428160981c7d4903ea1c44c5a2a9e5bd7a16
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-12 10:42:45 -0800

    Initialize empty bills collection

--- app/assets/javascripts/models/group.js
+++ app/assets/javascripts/models/group.js
@@ -1,6 +1,11 @@
 BT.Models.Group = Backbone.Model.extend({
   urlRoot: "/api/groups",
   collection: BT.Collections.Groups,
+
+  initialize: function () {
+    this.bills = new BT.Collections.Bills();
+  },
+
   parse: function (serverAttrs) {
     if (serverAttrs.bills) {
       this.bills = new BT.Collections.Bills(serverAttrs.bills);
