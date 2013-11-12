ceab352741dc6435bf21f056a292bc383a672dd8
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-11 17:38:22 -0800

    Add initial group detail template

--- app/assets/javascripts/views/groups/group_detail_view.js
+++ app/assets/javascripts/views/groups/group_detail_view.js
@@ -2,12 +2,17 @@ BT.Views.GroupDetailView = Backbone.View.extend({
   template: JST['groups/detail'],
 
   render: function () {
-    users = _.map(this.model.user_ids, function (user_id) {
+    var users = _.map(this.model.user_ids, function (user_id) {
       return BT.users.get(user_id);
+    });
+
+    var userDisplayNames = _.map(users, function(user) {
+      return user.displayName();
     });
 
     renderedContent = this.template({
       group: this.model,
+      displayNames: userDisplayNames,
       users: users,
       bills: this.model.bills
     });
