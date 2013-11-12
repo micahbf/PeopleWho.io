3710d497af4082c12071bf6e825899ea9c7e31c6
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-12 10:00:30 -0800

    Add #addNewGroupForm and events

--- app/assets/javascripts/views/groups/group_panel_view.js
+++ app/assets/javascripts/views/groups/group_panel_view.js
@@ -1,6 +1,11 @@
 BT.Views.GroupsPanelView = Backbone.View.extend({
   template: JST['groups/panel'],
 
+  events: {
+    "click #new-group-btn": "addNewGroupForm",
+    "blur #new-group-name": "createNewGroup"
+  },
+
   render: function () {
     renderedContent = this.template({
       groups: BT.groups
@@ -10,5 +15,17 @@ BT.Views.GroupsPanelView = Backbone.View.extend({
     this.$el.addClass("panel panel-default");
 
     return this;
+  },
+
+  addNewGroupForm: function(event) {
+    var $newGroupLi, $nameInput;
+    event.preventDefault();
+
+    $newGroupLi = $("<li class='list-group-item'></li>");
+    $nameInput = $("<input type='text' id='new-group-name' class='form-control'>");
+    $newGroupLi.append($nameInput);
+
+    this.$el.find(".list-group").append($newGroupLi);
+    $nameInput.focus();
   }
 });
