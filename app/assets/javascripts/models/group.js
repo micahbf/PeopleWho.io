d97b2f795d1d1fa5f33a66d1712068c012eec418
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-11 17:50:12 -0800

    Write #parse to parse bills into collection

--- app/assets/javascripts/models/group.js
+++ app/assets/javascripts/models/group.js
@@ -1,4 +1,12 @@
 BT.Models.Group = Backbone.Model.extend({
   urlRoot: "/api/groups",
-  collection: BT.Collections.Groups
+  collection: BT.Collections.Groups,
+  parse: function (serverAttrs) {
+    if (serverAttrs.group.bills) {
+      this.bills = new BT.Collections.Bills(serverAttrs.group.bills);
+      delete serverAttrs.group.bills;
+    }
+
+    return serverAttrs;
+  }
 });
