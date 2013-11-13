d7400cf8c5ea09b491fd80bbe4cffad9f274d86e
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-13 14:40:34 -0800

    Generate and migrate delayed_job:active_record

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131113214443) do
+ActiveRecord::Schema.define(:version => 20131113223954) do
 
   create_table "bill_splits", :force => true do |t|
     t.integer  "bill_id",     :null => false
@@ -49,6 +49,22 @@ ActiveRecord::Schema.define(:version => 20131113214443) do
   end
 
   add_index "currencies", ["code"], :name => "index_currencies_on_code", :unique => true
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0, :null => false
+    t.integer  "attempts",   :default => 0, :null => false
+    t.text     "handler",                   :null => false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "user_group_memberships", :force => true do |t|
     t.integer  "user_id"
