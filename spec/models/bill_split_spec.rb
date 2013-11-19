1a78dd5680ea51ae1fe8c6f2d0ed34193338f92e
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-18 17:00:41 -0800

    Add validation specs

--- spec/models/bill_split_spec.rb
+++ spec/models/bill_split_spec.rb
@@ -1,4 +1,31 @@
 require 'spec_helper'
 
 describe BillSplit do
+  describe "Validations" do
+    subject(:bill_split) { FactoryGirl.build(:bill_split) }
+
+    it "has a valid factory" do
+      expect(bill_split).to be_valid
+    end
+
+    it "must have an amount" do
+      bill_split.amount = nil
+      expect(bill_split).not_to be_valid
+    end
+
+    it "amount must be an integer" do
+      bill_split.amount = "anid"
+      expect(bill_split).not_to be_valid
+    end
+
+    it "must have an associated bill" do
+      bill_split.bill_id = nil
+      expect(bill_split).not_to be_valid
+    end
+
+    it "must have an associated debtor" do
+      bill_split.debtor_id = nil
+      expect(bill_split).not_to be_valid
+    end
+  end
 end
