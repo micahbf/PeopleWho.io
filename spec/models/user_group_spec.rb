1614fe67c36ff6e4286dac8233d2b5a00e401ca0
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-19 09:36:59 -0800

    Write validation specs for UserGroup

--- spec/models/user_group_spec.rb
+++ spec/models/user_group_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe UserGroup do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    subject(:user_group) { FactoryGirl.build(:user_group) }
+
+    it "has a valid factory" do
+      expect(user_group).to be_valid
+    end
+
+    it "must have a name" do
+      user_group.name = nil
+      expect(user_group).not_to be_valid
+    end
+  end
 end
