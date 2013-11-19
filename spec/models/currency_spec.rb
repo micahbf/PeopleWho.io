680dfc848fc6e0df17a2675a76d2b17fd8399dd2
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-19 09:56:09 -0800

    Add validation specs

--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe Currency do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    subject(:currency) { FactoryGirl.build(:currency) }
+
+    it "has a valid factory" do
+      expect(currency).to be_valid
+    end
+
+    it "must have a currency code" do
+      currency.code = nil
+      expect(currency).not_to be_valid
+    end
+
+    it "must have a name" do
+      currency.full_name = nil
+      expect(currency).not_to be_valid
+    end
+
+    it "must have a rate" do
+      currency.rate = nil
+      expect(currency).not_to be_valid
+    end
+
+    it "rate must be a number" do
+      currency.rate = "abc"
+      expect(currency).not_to be_valid
+    end
+  end
 end
