b1afd952bd7cd065b9e9a118a652d528bb5708c2
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-11-19 10:02:54 -0800

    Add validation specs

--- spec/models/user_group_membership_spec.rb
+++ spec/models/user_group_membership_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe UserGroupMembership do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    subject(:membership) { FactoryGirl.build(:user_group_membership) }
+
+    it "has a valid factory" do
+      expect(membership).to be_valid
+    end
+
+    it "must have a user" do
+      membership.user_id = nil
+      expect(membership).not_to be_valid
+    end
+
+    it "must have a group" do
+      membership.group_id = nil
+      expect(membership).not_to be_valid
+    end
+
+    it "must have a unique user-group combination" do
+      other_membership = FactoryGirl.create(:user_group_membership,
+                                            group_id: membership.group_id,
+                                            user_id: membership.user_id)
+
+      expect(membership).not_to be_valid
+    end
+  end
 end
